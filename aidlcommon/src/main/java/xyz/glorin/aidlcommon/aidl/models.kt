50b8fed96f0819bd82ad3b2cf31122b2188a4dce
glorinli <ligr1991@gmail.com>
2019-11-20 16:47:29 +0800

    Add common module.

---
+++ aidlcommon/src/main/java/xyz/glorin/aidlcommon/aidl/models.kt
@@ -0,0 +1,30 @@
+package xyz.glorin.aidlcommon.aidl
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class Album(val title: String?, val count: Int) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readInt()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeInt(count)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Album> {
+        override fun createFromParcel(parcel: Parcel): Album {
+            return Album(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Album?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
---
+++ aidlservice/src/main/java/xyz/glorin/aidlservice/AlbumService.kt
@@ -0,0 +1,12 @@
+package xyz.glorin.aidlservice
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class AlbumService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+}
